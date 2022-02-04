a6227b10d0f968d67834af8070b4b2c3ec0a632b
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2022-02-04 11:52:35 +0200

    added a spritesheet, text and removed a font

--- src/game.js
+++ src/game.js
@@ -3,8 +3,32 @@ const scene = {
         this.load.bitmapFont("arcade", "/assets/font/arcade.png", "/assets/font/arcade.xml");
     },
     create: function () {
-    this.add.bitmapText(400, 300, "arcade", "Hello Phaser").setOrigin(0.5);
-} }
+        this.helloText = this.add.bitmapText(400, 300, "arcade", "Hello Phaser").setOrigin(0.5);
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }, 
+    update: function () {
+        //this.helloText.x += 10
+        if (this.cursors.left.isDown) {
+            this.helloText.x -= 10;
+        }
+        if (this.cursors.right.isDown) {
+            this.helloText.x += 10;
+}
+        if (this.cursors.up.isDown) {
+            this.helloText.y -= 10;
+}
+        if (this.cursors.down.isDown) {
+            this.helloText.y += 10;
+}
+        if (this.helloText.x > 1000) {
+            this.helloText.x = -200
+        }
+
+        if (this.helloText.x < -210) {
+            this.helloText.x = 990
+        }
+    } 
+}
 
 const config = {
     type: Phaser.AUTO,
